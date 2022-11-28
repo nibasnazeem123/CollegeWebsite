244aa67b6b8cc7d5425e4fb1d825fb63cda1e16a
nibasnazeem123 <117911360+nibasnazeem123@users.noreply.github.com>
2022-11-28 18:35:10 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CourseComponent } from './course/course.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+{ path: 'Home', component: HomeComponent },
+{ path: 'About', component: AboutUsComponent },
+{ path: 'Contact', component: ContactUsComponent },
+{ path: 'Course', component: CourseComponent },
+{ path: 'Gallery', component: GalleryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CourseComponent } from './course/course.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    GalleryComponent,
+    CourseComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
